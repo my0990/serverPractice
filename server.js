b170b2252e4349c02911a32dd8a8448f120aa879
my0990 <my0990@naver.com>
2021-08-17 17:35:40 +0900

    login function

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const MongoClient = require('mongodb').MongoClient;
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
 
 var db;
 MongoClient.connect('mongodb+srv://admin:qwer1234@cluster0.i0oe3.mongodb.net/Todoapp?retryWrites=true&w=majority',function(err,client){
@@ -88,3 +95,57 @@ app.put('/edit',function(req,res){
     });
 })
 
+app.get('/login',function(req,res){
+    res.render('login.ejs')
+})
+
+app.get('/fail',function(req,res){
+    res.render('fail.ejs')
+})
+
+app.post('/login',  passport.authenticate('local', {failureRedirect : '/fail'}),function(req,res){
+    res.redirect('/')
+})
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (입력한아이디, 입력한비번, done) {
+    console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+      if (에러) return done(에러)
+  
+      if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+      if (입력한비번 == 결과.pw) {
+        return done(null, 결과)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }
+    })
+  }));
+
+  app.get('/mypage',로그인했니, function(req,res){
+      console.log(req.user)
+      res.render('mypage.ejs', { 사용자: req.user})
+  })
+
+function 로그인했니(res, req, next) {
+  if (res.user) {
+    next()
+  } else {
+    req.send('로그인안하셨는데요?')
+  }
+}
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  });
+  
+  passport.deserializeUser(function (아이디, done) {
+      db.collection('login').findOne({id: 아이디},function(에러,결과){
+        done(null, 결과)
+      })
+    
+  }); 
